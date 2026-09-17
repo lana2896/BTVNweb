b54ece86cc92ecb42bafae62474b8c127c2de668
lana2896 <minhthu.08092006@gmail.com>
2026-09-17 15:05:39 +0700

    bai 3 - crud categort API + bai 4 swagger (springdoc)

---
+++ src/main/java/vn/iotstar/Controller/api/HomeController.java
@@ -0,0 +1,13 @@
+package vn.iotstar.Controller.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String home() {
+		return "redirect:/swagger-ui.html";
+	}
+}
